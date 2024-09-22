dd103d5f2d59229ee6054c728ef6e07d5b03c5b8
MarkusJLechner <markus@hellocash.at>
2024-09-22 12:09:15 +0200

    refactor: run prettier

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <div class="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-5 flex flex-col items-center justify-center">
+  <div
+    class="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-5 flex flex-col items-center justify-center"
+  >
     <!-- Main Container -->
     <div
       :class="[
-    'relative rounded-2xl shadow-xl p-[2px] max-w-md w-full overflow-hidden flex flex-col items-center bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg min-h-[230px]',
-    isRunning ? 'border-animation' : '',
-  ]"
+        'relative rounded-2xl shadow-xl p-[2px] max-w-md w-full overflow-hidden flex flex-col items-center bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg min-h-[230px]',
+        isRunning ? 'border-animation' : ''
+      ]"
     >
-      <div class="relative rounded-2xl shadow-xl p-6 max-w-md h-full w-full overflow-hidden flex flex-col items-center bg-gradient-to-tl from-[#C44ECB] to-[#895DF4] ">
+      <div
+        class="relative rounded-2xl shadow-xl p-6 max-w-md h-full w-full overflow-hidden flex flex-col items-center bg-gradient-to-tl from-[#C44ECB] to-[#895DF4]"
+      >
         <!-- Timer Display -->
         <div class="text-center text-white text-4xl font-bold mb-6">
           {{ currentTimer }}
-          <span v-if="isRunning" class="text-sm block mt-2">Current: <span class="text-yellow-300">{{ timerEvents[timerEvents.length - 1].details }}</span></span>
+          <span v-if="isRunning" class="text-sm block mt-2"
+            >Current:
+            <span class="text-yellow-300">{{
+              timerEvents[timerEvents.length - 1].details
+            }}</span></span
+          >
         </div>
         <!-- Task Input -->
         <div class="mb-4 w-full">
@@ -30,7 +39,13 @@
             class="rounded-full bg-green-500 p-4 shadow-lg text-white hover:bg-green-600 focus:outline-none transition transform hover:scale-105"
           >
             <!-- Play Icon -->
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
               <path d="M5 3v18l15-9L5 3z" />
             </svg>
           </button>
@@ -39,25 +54,37 @@
             class="rounded-full bg-red-500 p-4 shadow-lg text-white hover:bg-red-600 focus:outline-none transition transform hover:scale-105"
           >
             <!-- Stop Icon -->
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
               <rect x="6" y="6" width="12" height="12" />
             </svg>
           </button>
         </div>
       </div>
-
     </div>
     <!-- Events and Summary Section -->
     <div class="flex flex-wrap justify-center w-full max-w-4xl mt-4 gap-4">
       <!-- Events List -->
-      <div class="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-2xl shadow-xl p-4 w-full max-w-sm">
+      <div
+        class="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-2xl shadow-xl p-4 w-full max-w-sm"
+      >
         <div class="flex justify-between items-center mb-2">
           <div class="text-white text-xl font-semibold">Events</div>
-          <button @click="clearEvents" class="text-white underline focus:outline-none text-sm">Clear</button>
+          <button @click="clearEvents" class="text-white underline focus:outline-none text-sm">
+            Clear
+          </button>
         </div>
         <ul class="text-white overflow-y-auto max-h-40 space-y-2">
-          <li v-for="(event, index) in reversedTimerEvents" :key="index"
-              class="p-3 rounded-lg bg-white bg-opacity-20 text-sm">
+          <li
+            v-for="(event, index) in reversedTimerEvents"
+            :key="index"
+            class="p-3 rounded-lg bg-white bg-opacity-20 text-sm"
+          >
             <div class="flex justify-between items-center">
               <div class="font-semibold">Task:</div>
               <div class="text-yellow-300 truncate">{{ event.details }}</div>
@@ -72,15 +99,19 @@
             </div>
             <div class="flex justify-between items-center mt-1">
               <div class="font-semibold">Duration:</div>
-              <div class="text-yellow-300">{{ formatDuration(event.startTime, event.endTime) }}</div>
+              <div class="text-yellow-300">
+                {{ formatDuration(event.startTime, event.endTime) }}
+              </div>
             </div>
           </li>
         </ul>
       </div>
       <!-- Summary Section -->
-      <div class="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-2xl shadow-xl p-4 w-full max-w-sm">
+      <div
+        class="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-2xl shadow-xl p-4 w-full max-w-sm"
+      >
         <div class="text-white text-xl font-semibold mb-2">Summary</div>
-        <ul class="text-white space-y-1 text-sm overflow-y-auto max-h-40 ">
+        <ul class="text-white space-y-1 text-sm overflow-y-auto max-h-40">
           <li v-for="(duration, tag) in tagDurations" :key="tag" class="flex justify-between">
             <div>{{ tag }}:</div>
             <div class="text-yellow-300">{{ formatDuration(0, duration) }}</div>
@@ -92,9 +123,8 @@
 </template>
 
 <script setup lang="ts">
-import Multiselect from 'vue-multiselect'
 import 'vue-multiselect/dist/vue-multiselect.min.css'
-import { nextTick, onMounted, ref,watch, computed } from 'vue'
+import { nextTick, onMounted, ref, watch, computed } from 'vue'
 import { useLocalStorage } from '@vueuse/core'
 
 const taskInput = ref('')
@@ -102,7 +132,7 @@ const currentTimer = ref('00:00:00')
 const isRunning = ref(false)
 let intervalId: number | null = null
 
-const timerEvents = useLocalStorage<{ startTime: number, details: string }[]>('timerEvents', [])
+const timerEvents = useLocalStorage<{ startTime: number; details: string }[]>('timerEvents', [])
 
 const tagDurations = ref<{ [key: string]: number }>({})
 
@@ -112,12 +142,12 @@ watch(timerEvents, calculateTagDurations, { deep: true })
 function calculateTagDurations() {
   const durations: { [key: string]: number } = {}
 
-  timerEvents.value.forEach(event => {
+  timerEvents.value.forEach((event) => {
     if (event.details.includes('#')) {
       const tags = event.details.match(/#\w+/g) || []
       const duration = event.endTime ? event.endTime - event.startTime : 0
 
-      tags.forEach(tag => {
+      tags.forEach((tag) => {
         if (!durations[tag]) {
           durations[tag] = 0
         }
@@ -149,7 +179,9 @@ function startTimer() {
 
   intervalId = setInterval(() => {
     const currentTime = Date.now()
-    const elapsedTime = Math.floor((currentTime - timerEvents.value[timerEvents.value.length - 1].startTime) / 1000)
+    const elapsedTime = Math.floor(
+      (currentTime - timerEvents.value[timerEvents.value.length - 1].startTime) / 1000
+    )
     const hrs = String(Math.floor(elapsedTime / 3600)).padStart(2, '0')
     const mins = String(Math.floor((elapsedTime % 3600) / 60)).padStart(2, '0')
     const secs = String(elapsedTime % 60).padStart(2, '0')
@@ -182,11 +214,11 @@ function clearEvents() {
 
 function formatDuration(startTime: number, endTime: number): string {
   endTime ??= Date.now()
-  const duration = Math.floor((endTime - startTime) / 1000);
-  const hrs = String(Math.floor(duration / 3600)).padStart(2, '0');
-  const mins = String(Math.floor((duration % 3600) / 60)).padStart(2, '0');
-  const secs = String(duration % 60).padStart(2, '0');
-  return `${hrs}:${mins}:${secs}`;
+  const duration = Math.floor((endTime - startTime) / 1000)
+  const hrs = String(Math.floor(duration / 3600)).padStart(2, '0')
+  const mins = String(Math.floor((duration % 3600) / 60)).padStart(2, '0')
+  const secs = String(duration % 60).padStart(2, '0')
+  return `${hrs}:${mins}:${secs}`
 }
 
 onMounted(() => {
@@ -203,16 +235,11 @@ function focusInput() {
 </script>
 
 <style scoped>
-
 .border-animation::before {
   content: '';
   position: absolute;
   inset: -160px;
-  background: conic-gradient(
-    transparent 270deg,
-    #ffff00,
-    transparent
-  );
+  background: conic-gradient(transparent 270deg, #ffff00, transparent);
   animation: rotate 4s linear infinite;
   z-index: -1;
 }
@@ -227,18 +254,21 @@ function focusInput() {
 }
 
 .neumorphism {
-  box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.8),
-  -20px -20px 50px rgba(255, 255, 255, 0.05);
+  box-shadow:
+    20px 20px 50px rgba(0, 0, 0, 0.8),
+    -20px -20px 50px rgba(255, 255, 255, 0.05);
 }
 
 .neumorphism-input {
-  box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.6),
-  inset -5px -5px 10px rgba(255, 255, 255, 0.1);
+  box-shadow:
+    inset 5px 5px 10px rgba(0, 0, 0, 0.6),
+    inset -5px -5px 10px rgba(255, 255, 255, 0.1);
 }
 
 .neumorphism-button {
-  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.5),
-  0 -10px 15px rgba(255, 255, 255, 0.05);
+  box-shadow:
+    0 10px 15px rgba(0, 0, 0, 0.5),
+    0 -10px 15px rgba(255, 255, 255, 0.05);
 }
 
 /* Custom scrollbar styles */
